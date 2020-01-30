0a61e5f9b875a94795e21f5319459d7f11f6ee9c
Humberto Hernandez <hamburger45@gmail.com>
2020-01-29 23:01:01 -0800

    started implementation of GET Pokemon

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
@@ -26,12 +28,30 @@ const validTypes = [
 	`Water`
 ];
 
+app.use(function validateBearerToken(req, res, next) {
+	const apiToken = process.env.API_TOKEN;
+	const authToken = req.get('Authorization');
+
+	if (!authToken || authToken.split(' ')[1] !== apiToken) {
+		return res.status(401).json({ error: 'Unathorized request' });
+	}
+	// move to the next middleware
+	next();
+});
 // callback function aka middleware
 function handleGetTypes(req, res) {
 	res.json(validTypes);
 }
+
 // constructs end point
 app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+	res.json(POKEDEX);
+	console.log(POKEDEX);
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 const PORT = 8000;
 
