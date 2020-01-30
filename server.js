faf771751407de9fbe3b2efec9bbfb53dbab70d6
Humberto Hernandez <hamburger45@gmail.com>
2020-01-29 21:08:02 -0800

    added endpoit handle function and pokemon types

--- server.js
+++ server.js
@@ -4,10 +4,34 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
+// pokemon types
+const validTypes = [
+	`Bug`,
+	`Dark`,
+	`Dragon`,
+	`Electric`,
+	`Fairy`,
+	`Fighting`,
+	`Fire`,
+	`Flying`,
+	`Ghost`,
+	`Grass`,
+	`Ground`,
+	`Ice`,
+	`Normal`,
+	`Poison`,
+	`Psychic`,
+	`Rock`,
+	`Steel`,
+	`Water`
+];
 
-app.use((req, res) => {
-	res.send('Hello, world!');
-});
+// callback function aka middleware
+function handleGetTypes(req, res) {
+	res.json(validTypes);
+}
+// constructs end point
+app.get('/types', handleGetTypes);
 
 const PORT = 8000;
 
