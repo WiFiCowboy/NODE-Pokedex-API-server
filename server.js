fa2036bd2d802e1dd1992833ee7ced76a57f82b9
Humberto Hernandez <hamburger45@gmail.com>
2020-01-30 16:07:29 -0800

    completed

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ const POKEDEX = require('./pokedex.json');
 const app = express();
 
 app.use(morgan('dev'));
-// pokemon types
+
+app.use(function validateBearerToken(req, res, next) {
+	const apiToken = process.env.API_TOKEN;
+	const authToken = req.get('Authorization');
+
+	if (!authToken || authToken.split(' ')[1] !== apiToken) {
+		return res.status(401).json({ error: 'Unauthorized request' });
+	}
+	// move to the next middleware
+	next();
+});
+
 const validTypes = [
 	`Bug`,
 	`Dark`,
@@ -28,30 +39,28 @@ const validTypes = [
 	`Water`
 ];
 
-app.use(function validateBearerToken(req, res, next) {
-	const apiToken = process.env.API_TOKEN;
-	const authToken = req.get('Authorization');
-
-	if (!authToken || authToken.split(' ')[1] !== apiToken) {
-		return res.status(401).json({ error: 'Unathorized request' });
-	}
-	// move to the next middleware
-	next();
-});
-// callback function aka middleware
-function handleGetTypes(req, res) {
+app.get('/types', function handleGetTypes(req, res) {
 	res.json(validTypes);
-}
+});
 
-// constructs end point
-app.get('/types', handleGetTypes);
+app.get('/pokemon', function handleGetPokemon(req, res) {
+	let response = POKEDEX.pokemon;
 
-function handleGetPokemon(req, res) {
-	res.json(POKEDEX);
-	console.log(POKEDEX);
-}
+	// filter our pokemon by name if name query param is present
+	if (req.query.name) {
+		response = response.filter((pokemon) =>
+			// case insensitive searching
+			pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+		);
+	}
 
-app.get('/pokemon', handleGetPokemon);
+	// filter our pokemon by type if type query param is present
+	if (req.query.type) {
+		response = response.filter((pokemon) => pokemon.type.includes(req.query.type));
+	}
+
+	res.json(response);
+});
 
 const PORT = 8000;
 
